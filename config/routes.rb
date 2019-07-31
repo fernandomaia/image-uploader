22378c4b8eb617bcfefc061fb5d26f185a494486
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2019-07-31 20:11:30 -0300

    Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'image_files#new'
   resources :image_files
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
