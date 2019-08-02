00131601467907febda526a2c171af5424234043
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2019-08-01 21:39:22 -0300

    Add activestorage-validator gem to validate image file

--- app/models/image_file.rb
+++ app/models/image_file.rb
@@ -1,4 +1,8 @@
 class ImageFile < ApplicationRecord
-
   has_one_attached :image
+
+  validates :owner, presence: true
+  validates :description, presence: true
+  validates :image, presence: true, blob: { content_type: ['image/png', 'image/gif', 'image/jpeg'] }
+
 end
