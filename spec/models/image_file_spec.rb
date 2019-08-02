6e515af70d0895fb5cf64b37b659c061280cf355
Fernando Maia <2966576+fernandomaia@users.noreply.github.com>
2019-08-01 21:44:28 -0300

    Attach example image to test block

--- spec/models/image_file_spec.rb
+++ spec/models/image_file_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe ImageFile, type: :model do
   it 'is valid with valid attributes' do
     image_file = ImageFile.new(owner: 'Person', description: 'Text')
+    image_file.image.attach(io: File.open(Rails.root.join('spec', 'files', 'lock.png')),
+      filename: 'lock.png', content_type: 'image/png')
     expect(image_file).to be_valid
   end
 
